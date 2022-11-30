eb9a21e686ae4ed38ae6b86a26dd792fc0ae830a
Alen Koso <a.koso@sportradar.com>
2022-11-30 21:47:20 +0100

    Updating script.

--- utils/getInput.py
+++ utils/getInput.py
@@ -23,7 +23,7 @@ def arg_to_int(i, name):
 
 
 def get_single_input(input_year, input_day):
-    dir_name = os.path.join(BASE_DIR, "aoc", str(input_year), f"Day{input_day:02}")
+    dir_name = os.path.join(BASE_DIR, "../src", str(input_year), f"Day-{input_day:02}")
     cookies = {}
 
     try:
@@ -52,7 +52,7 @@ def get_single_input(input_year, input_day):
 
 def get_all_inputs(input_year):
     for inputDay in range(1, 26):
-        dir_name = os.path.join(BASE_DIR, "aoc", str(input_year), f"Day{inputDay:02}")
+        dir_name = os.path.join(BASE_DIR, "../src", str(input_year), f"Day-{inputDay:02}")
         cookies = {}
 
         try:
