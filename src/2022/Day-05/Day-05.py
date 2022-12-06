d7353b53d4e19f08bac0fd968167461e26ca2d6a
Alen Koso <a.koso@sportradar.com>
2022-12-06 11:35:41 +0100

    Day 05 solution

---
+++ src/2022/Day-05/Day-05.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+from copy import deepcopy
+DATA = open("input.txt").read()
+lines = [x for x in DATA.split('\n')]
+
+
+solution = []
+commands = []
+for line in lines:
+    if line != '':
+        sz = (len(line)+1)//4
+        while len(solution) < sz:
+            solution.append([])
+        for i in range(len(solution)):
+            ch = line[1 + 4 * i]
+            if 'A' <= ch <= 'Z' and ch != ' ':
+                solution[i].append(ch)
+        continue
+    break
+
+part_one = deepcopy(solution)
+part_two = deepcopy(solution)
+found = False
+for command in lines:
+    if command == '':
+        found = True
+        continue
+    if not found:
+        continue
+
+    # Command: move 3 from 5 to 2
+    words = command.split()
+    quantity = int(words[1])
+    from_ = int(words[3]) - 1
+    to_ = int(words[5]) - 1
+    for (ST, do_rev) in [(part_one, True), (part_two, False)]:
+        MOVE = ST[from_][:quantity]
+        ST[from_] = ST[from_][quantity:]
+        ST[to_] = (list(reversed(MOVE)) if do_rev else MOVE) + ST[to_]
+print('Part 1: ' + ''.join([s[0] for s in part_one if len(s) > 0]))
+print('Part 2: ' + ''.join([s[0] for s in part_two if len(s) > 0]))
