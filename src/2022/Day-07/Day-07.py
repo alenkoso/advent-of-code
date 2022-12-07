b4b8d8f8ed7936f92df44cc3dab914e7ea3cc5c4
Alen Koso <a.koso@sportradar.com>
2022-12-07 14:00:02 +0100

    Day 07 refactor.

--- src/2022/Day-07/Day-07.py
+++ src/2022/Day-07/Day-07.py
@@ -14,9 +14,13 @@ def parse_input_and_build_directory():
         if len(line.strip()) == 0:
             continue
         if line[0] == '$':
-            c, cmd, *args = line.split()
-            if cmd == 'cd':
+            c, command, *args = line.split()
+
+            # cd means change directory. This changes which directory is the current directory,
+            # but the specific result depends on the argument
+            if command == 'cd':
                 path, = args
+                # cd / switches the current directory to the outermost directory, /.
                 if path[0] == '/':
                     current_directory = path
                 else:
@@ -25,9 +29,9 @@ def parse_input_and_build_directory():
                     directories[current_directory] = 0
                     sub_directories[current_directory] = []
         else:
-            sz, file_name = line.split()
-            if sz != 'dir':
-                directories[current_directory] += int(sz)
+            size, file_name = line.split()
+            if size != 'dir':
+                directories[current_directory] += int(size)
             else:
                 sub_directories[current_directory].append(os.path.normpath(os.path.join(current_directory, file_name)))
 
@@ -43,9 +47,9 @@ def get_directory_size(directory_name):
 def part_one():
     total_size = 0
     for directory in directories:
-        dsize = get_directory_size(directory)
-        if dsize <= 100000:
-            total_size += dsize
+        current_directory_size = get_directory_size(directory)
+        if current_directory_size <= 100000:
+            total_size += current_directory_size
     return total_size
 
 
@@ -53,8 +57,8 @@ def part_two():
     total_size = get_directory_size('/')
     unused_space = 70000000 - total_size
     result_dir_size = None
-    for d in directories:
-        current_dir_size = get_directory_size(d)
+    for directory in directories:
+        current_dir_size = get_directory_size(directory)
         if unused_space + current_dir_size >= 30000000:
             if result_dir_size is None or result_dir_size > current_dir_size:
                 result_dir_size = current_dir_size
