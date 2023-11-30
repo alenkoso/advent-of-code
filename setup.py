818952fdfeeeda8670330c1a24484f3ab39052b9
Alen Koso <a.koso@sportradar.com>
2023-11-30 12:01:38 +0100

    feat: extracting duplicate code from fetch functions and creating new ones to make code easier to maintain.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_session_cookie():
     return config['DEFAULT']['SessionCookie']
 
 def create_directory(year, day):
-    """Create directories for the given year and day under './src/' if they don't exist."""
+    # Create directories for the given year and day under './src/' if they don't exist.
     base_dir = "./src"
     year_dir = os.path.join(base_dir, str(year))
     day_dir = os.path.join(year_dir, f"Day_{day}")
@@ -29,41 +29,41 @@ def create_directory(year, day):
 
     return day_dir
 
+def generate_aoc_url(year, day, is_input=False):
+    # Generate the URL for the given day and year for Advent of Code.
+    base_url = f"https://adventofcode.com/{year}/day/{day}"
+    return f"{base_url}/input" if is_input else base_url
+
+def make_request(url, session_cookie=None):
+    # Make an HTTP request to the given URL.
+    headers = {'Cookie': f'session={session_cookie}'} if session_cookie else {}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response
+
 def fetch_input(day, year, session_cookie):
-    """Fetch the puzzle input for the given day and year using the session cookie."""
-    url = f"https://adventofcode.com/{year}/day/{day}/input"
-    cookies = {'session': session_cookie}
-    response = requests.get(url, cookies=cookies)
-    response.raise_for_status()  # This will raise an error if the fetch fails
+    # Fetch the puzzle input for the given day and year using the session cookie.
+    url = generate_aoc_url(year, day, is_input=True)
+    response = make_request(url, session_cookie)
     return response.text
 
 def save_input(input_data, dir_name):
-    """Save the fetched input data to a text file."""
+    # Save the fetched input data to a text file.
     with open(os.path.join(dir_name, "input.txt"), 'w') as file:
         file.write(input_data)
 
 def fetch_instructions(day, year):
-    """Fetch the puzzle instructions for the given day and year."""
-    url = f"https://adventofcode.com/{year}/day/{day}"
-    response = requests.get(url)
-    response.raise_for_status()  # Ensure the request was successful
-
-    # Use BeautifulSoup to parse the HTML content
+    # Fetch the puzzle instructions for the given day and year.
+    url = generate_aoc_url(year, day)
+    response = make_request(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Assuming the instructions are contained in an <article> tag
-    # Modify this as per the actual structure of the webpage
     article = soup.find('article')
-    if article:
-        return article.get_text(strip=True)
-    else:
-        return "Instructions not found."
+    return article.get_text(strip=True) if article else "Instructions not found."
 
 def save_instructions(instructions, dir_name):
-    """Save the instructions to a text file."""
+    # Save the instructions to a text file.
     with open(os.path.join(dir_name, "instructions.txt"), 'w') as file:
         file.write(instructions)
-
 
 def main():
     SESSION_COOKIE = get_session_cookie()
