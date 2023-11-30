785ccf2b65a14f25bdad975972ff2e1e3f6e0516
Alen Koso <a.koso@sportradar.com>
2023-11-30 13:36:38 +0100

    fix: saw some hardcoded values, fixing.

--- setup/setup.py
+++ setup/setup.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from BeautifulSoup import bs4
+# from BeautifulSoup import bs4
 from datetime import datetime
 import configparser
 
@@ -91,14 +91,14 @@ def setup_day_challenge(current_day, current_year, session_cookie):
 
     try:
         # Fetch and save inputs
-        input_data = fetch_input(22, 2022, session_cookie)
+        input_data = fetch_input(current_day, current_year, session_cookie)
         save_input(input_data, day_dir)
         print(f"Input for Day {current_day}, Year {current_year} saved to {day_dir}/input.txt")
 
         # Fetch and save instructions
-        instructions = fetch_instructions(current_day, current_year)
-        save_instructions(instructions, day_dir)
-        print(f"Instructions for Day {current_day}, Year {current_year} saved to {day_dir}/instructions.txt")
+        # instructions = fetch_instructions(current_day, current_year)
+        # save_instructions(instructions, day_dir)
+        # print(f"Instructions for Day {current_day}, Year {current_year} saved to {day_dir}/instructions.txt")
 
         # Create base Python file
         create_base_python_file(current_day, day_dir)
