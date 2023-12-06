6b2cbf42bbd93cccd6977734e67f60145e97ba55
Alen Koso <a.koso@sportradar.com>
2023-12-06 06:59:15 +0100

    feat: 2023 Day 6 - part 2 fixed. added execution times, optimised a bit.

--- src/2023/Day_6/6.py
+++ src/2023/Day_6/6.py
@@ -1,69 +1,46 @@
 import os
 import sys
+import time
 
 project_root = os.path.join(os.path.dirname(__file__), '..', '..', '..')
 sys.path.append(project_root)
 from helpers.file_utils import read_input_file
 
-# Part 1
-def calculate_winning_ways(race_times, record_distances):
-    winning_ways = []
-    for race_time, record_distance in zip(race_times, record_distances):
-        ways_to_win = 0
-        for hold_time in range(race_time):
-            speed = hold_time
-            travel_time = race_time - hold_time
-            distance = speed * travel_time
-            if distance > record_distance:
-                ways_to_win += 1
-        winning_ways.append(ways_to_win)
-    return winning_ways
-
-def parse_input(input_data):
-    race_times = list(map(int, input_data[0].split()[1:]))
-    record_distances = list(map(int, input_data[1].split()[1:]))
-    return race_times, record_distances
-
-# Part 2
-def calculate_winning_ways_single_race(race_time, record_distance):
-    optimal_hold_time = race_time // 2
+def calculate_ways_to_win(race_time, record_distance):
     ways_to_win = 0
-
-    # Start from the optimal hold time and decrease
-    for hold_time in range(optimal_hold_time, -1, -1):
-        distance = hold_time * (race_time - hold_time)
-        if distance > record_distance:
-            ways_to_win = race_time - hold_time
-            break
-
+    for hold_time in range(race_time + 1):
+        distance_travelled = hold_time * (race_time - hold_time)
+        if distance_travelled >= record_distance:
+            ways_to_win += 1
     return ways_to_win
 
-def parse_single_race_input(input_data):
-    race_time = int(input_data[0].replace("Time:", "").replace(" ", ""))
-    record_distance = int(input_data[1].replace("Distance:", "").replace(" ", ""))
-    return race_time, record_distance
+def parse_input(input_data):
+    race_times = [int(x) for x in input_data[0].split(':')[1].split()]
+    record_distances = [int(x) for x in input_data[1].split(':')[1].split()]
+    combined_race_time = int(''.join(map(str, race_times)))
+    combined_record_distance = int(''.join(map(str, record_distances)))
+    return race_times, record_distances, combined_race_time, combined_record_distance
 
 def main():
+    input_data = read_input_file('input.txt', mode='lines_stripped')
+
+    race_times, record_distances, combined_race_time, combined_record_distance = parse_input(input_data)
+
     # Part 1
-    input_data_multiple = [
-        "Time:      7  15   30",
-        "Distance:  9  40  200"
-    ]
-    race_times, record_distances = parse_input(input_data_multiple)
+    start_time = time.time()
     total_ways_multiple = 1
-    for ways in calculate_winning_ways(race_times, record_distances):
-        total_ways_multiple *= ways
-    print(f"Total ways to win all races: {total_ways_multiple}")
+    for each_race_time, each_record_distance in zip(race_times, record_distances):
+        total_ways_multiple *= calculate_ways_to_win(each_race_time, each_record_distance)
+    end_time = time.time()
+    print(f"Part 1: {total_ways_multiple}")
+    print(f"Part 1 Execution Time: {end_time - start_time} seconds")
 
     # Part 2
-    input_data_single = [
-        "Time:      71530",
-        "Distance:  940200"
-    ]
-    race_time, record_distance = parse_single_race_input(input_data_single)
-    total_ways_single = calculate_winning_ways_single_race(race_time, record_distance)
-    print(f"Total ways to win the single race: {total_ways_single}")
+    start_time = time.time()
+    total_ways_single = calculate_ways_to_win(combined_race_time, combined_record_distance)
+    end_time = time.time()
+    print(f"Part 2: {total_ways_single}")
+    print(f"Part 2 Execution Time: {end_time - start_time} seconds")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
