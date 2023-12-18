6bc2cfd260c750511b221bc2b70bd71d39a6f2bc
Alen Koso <a.koso@sportradar.com>
2023-12-18 05:39:45 +0100

    feat: 2023 Day 17 part 1 & part 2.

--- src/2023/Day_17/17.py
+++ src/2023/Day_17/17.py
@@ -1,37 +1,55 @@
 import heapq
+import time
 
-def read_input_file(file_path):
-    with open(file_path, 'r') as file:
-        return [[int(cell) for cell in line.strip()] for line in file]
+def solve(grid, min_dist, max_dist):
+    R, C = len(grid), len(grid[0])
+    DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # Right, Down, Left, Up
 
-def calculate_min_heat_loss(grid):
-    rows, cols = len(grid), len(grid[0])
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # Right, Down, Left, Up
-    heap = [(0, 0, 0, -1)]  # Heat loss, row, col, last direction index
-    visited = set()
+    q = [(0, 0, 0, -1, 0)]  # (cost, x, y, direction, distance in current direction)
+    seen = set()
+    costs = {}
 
-    while heap:
-        heat_loss, row, col, last_dir_idx = heapq.heappop(heap)
-        if (row, col) == (rows - 1, cols - 1):
-            return heat_loss
-        if (row, col, last_dir_idx) in visited:
+    while q:
+        cost, x, y, prev_dir, dist = heapq.heappop(q)
+        if (x, y) == (R - 1, C - 1):  # Reached destination
+            return cost
+        if (x, y, prev_dir) in seen:
             continue
-        visited.add((row, col, last_dir_idx))
+        seen.add((x, y, prev_dir))
 
-        for d_idx, (dr, dc) in enumerate(directions):
-            if d_idx == (last_dir_idx + 2) % 4:  # Skip reverse direction
+        for new_dir, (dx, dy) in enumerate(DIRS):
+            if new_dir == (prev_dir + 2) % 4:  # Prevent reversing direction
                 continue
+            if prev_dir != new_dir and dist < min_dist:  # Minimum distance check
+                continue
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < R and 0 <= ny < C:
+                new_cost = cost + int(grid[nx][ny])
+                new_dist = dist + 1 if new_dir == prev_dir else 1
+                if new_dist > max_dist or costs.get((nx, ny, new_dir), float('inf')) <= new_cost:
+                    continue
+                costs[(nx, ny, new_dir)] = new_cost
+                heapq.heappush(q, (new_cost, nx, ny, new_dir, new_dist))
 
-            new_row, new_col = row + dr, col + dc
-            if 0 <= new_row < rows and 0 <= new_col < cols:
-                new_heat_loss = heat_loss + grid[new_row][new_col]
-                heapq.heappush(heap, (new_heat_loss, new_row, new_col, d_idx))
+    return float('inf')  # In case no path is found
 
 def main():
-    input_file = "example.txt"
-    grid = read_input_file(input_file)
-    min_heat_loss = calculate_min_heat_loss(grid)
-    print(f"Minimum Heat Loss: {min_heat_loss}")
+    # Read input and process
+    # ...
+
+    # Part One
+    start_time = time.time()
+    min_heat_loss_normal = solve(grid, 1, 3)
+    print(f"Part One Result: {min_heat_loss_normal}")
+    end_time = time.time()
+    print(f"Part One Execution Time: {end_time - start_time} seconds")
+
+    # Part Two
+    start_time = time.time()
+    min_heat_loss_ultra = solve(grid, 4, 10)
+    print(f"Part Two Result: {min_heat_loss_ultra}")
+    end_time = time.time()
+    print(f"Part Two Execution Time: {end_time - start_time} seconds")
 
 if __name__ == "__main__":
     main()
