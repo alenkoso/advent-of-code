425eeb5ee2ef3c6edefd9bbf90c5cdb72497597f
Alen Koso <alenizljubljane@gmail.com>
2024-12-17 18:34:44 +0100

    feat(2024_day15): refactored day15

--- src/2024/Day_15/day15.py
+++ src/2024/Day_15/day15.py
@@ -1,130 +1,159 @@
+import os
+import sys
+import time
 from collections import deque
 
-def parse_input(file_path):
-    with open(file_path) as f:
-        raw = f.read().strip()
-    maze, path = raw.split("\n\n")
-    return maze.split("\n"), [x for x in path if x in "^v<>"]
+project_root = os.path.join(os.path.dirname(__file__), '..', '..', '..')
+sys.path.append(project_root)
 
-def get_robot_pos(maze):
-    for i, row in enumerate(maze):
-        if "@" in row:
-            return i, row.index("@")
+from helpers.parsing_utils import read_input_file_strip_lines
+
+def parse_input(lines):
+    # Split into layout and movement sequence
+    content = '\n'.join(lines)
+    layout, movements = content.split('\n\n')
+    return layout.split('\n'), [move for move in movements if move in "^v<>"]
+
+def find_robot_position(warehouse_layout):
+    for row_index, row in enumerate(warehouse_layout):
+        for col_index, cell in enumerate(row):
+            if cell == '@':
+                return row_index, col_index
     return 0, 0
 
-def move_robot(maze, pos_r, pos_c, dr, dc):
-    next_r = pos_r + dr
-    next_c = pos_c + dc
+def simulate_warehouse_movement(warehouse, robot_row, robot_col, direction_row, direction_col):
+    target_row = robot_row + direction_row
+    target_col = robot_col + direction_col
     
-    # Hit wall
-    if maze[next_r][next_c] == "#":
-        return pos_r, pos_c, maze
+    if warehouse[target_row][target_col] == "#":
+        return robot_row, robot_col, warehouse
         
-    # Empty space
-    if maze[next_r][next_c] == ".":
-        return next_r, next_c, maze
+    if warehouse[target_row][target_col] == ".":
+        return target_row, target_col, warehouse
     
-    # Find boxes to move
-    q = deque([(pos_r, pos_c)])
-    seen = set()
-    ok = True
+    movable_boxes = deque([(robot_row, robot_col)])
+    affected_positions = set()
+    movement_possible = True
     
-    while q:
-        rr, cc = q.popleft()
-        if (rr, cc) in seen:
+    while movable_boxes:
+        current_row, current_col = movable_boxes.popleft()
+        if (current_row, current_col) in affected_positions:
             continue
-        seen.add((rr, cc))
+            
+        affected_positions.add((current_row, current_col))
+        next_row = current_row + direction_row
+        next_col = current_col + direction_col
+
+        current_cell = warehouse[next_row][next_col]
         
-        rrr, ccc = rr + dr, cc + dc
-        if maze[rrr][ccc] == "#":
-            ok = False
+        if current_cell == "#":
+            movement_possible = False
             break
             
-        if maze[rrr][ccc] == "O":
-            q.append((rrr, ccc))
-        elif maze[rrr][ccc] == "[":
-            q.append((rrr, ccc))
-            if maze[rrr][ccc+1] != "]":
-                ok = False
+        if current_cell == "O":
+            movable_boxes.append((next_row, next_col))
+        elif current_cell == "[":
+            movable_boxes.append((next_row, next_col))
+            if warehouse[next_row][next_col + 1] != "]":
+                movement_possible = False
                 break
-            q.append((rrr, ccc+1))
-        elif maze[rrr][ccc] == "]":
-            q.append((rrr, ccc))
-            if maze[rrr][ccc-1] != "[":
-                ok = False
+            movable_boxes.append((next_row, next_col + 1))
+        elif current_cell == "]":
+            movable_boxes.append((next_row, next_col))
+            if warehouse[next_row][next_col - 1] != "[":
+                movement_possible = False
                 break
-            q.append((rrr, ccc-1))
+            movable_boxes.append((next_row, next_col - 1))
     
-    if not ok:
-        return pos_r, pos_c, maze
+    if not movement_possible:
+        return robot_row, robot_col, warehouse
     
-    # Move boxes
-    new_maze = [list(row) for row in maze]
-    seen.remove((pos_r, pos_c))
+    new_warehouse = [list(row) for row in warehouse]
+    affected_positions.remove((robot_row, robot_col))
     
-    while seen:
-        for rr, cc in sorted(seen):
-            rrr, ccc = rr + dr, cc + dc
-            if (rrr, ccc) not in seen and new_maze[rrr][ccc] == ".":
-                new_maze[rrr][ccc] = new_maze[rr][cc]
-                new_maze[rr][cc] = "."
-                seen.remove((rr, cc))
+    while affected_positions:
+        for pos_row, pos_col in sorted(affected_positions):
+            next_row = pos_row + direction_row
+            next_col = pos_col + direction_col
+            if (next_row, next_col) not in affected_positions and new_warehouse[next_row][next_col] == ".":
+                new_warehouse[next_row][next_col] = new_warehouse[pos_row][pos_col]
+                new_warehouse[pos_row][pos_col] = "."
+                affected_positions.remove((pos_row, pos_col))
                 break
     
-    return next_r, next_c, ["".join(row) for row in new_maze]
+    return target_row, target_col, ["".join(row) for row in new_warehouse]
 
-def part1(maze, path):
-    moves = {"^": (-1, 0), "v": (1, 0), "<": (0, -1), ">": (0, 1)}
-    rr, cc = get_robot_pos(maze)
-    maze = [list(row) for row in maze]
-    maze[rr][cc] = "."
-    maze = ["".join(row) for row in maze]
-    
-    for step in path:
-        rr, cc, maze = move_robot(maze, rr, cc, *moves[step])
-    
-    total = 0
-    for r in range(len(maze)):
-        for c in range(len(maze[0])):
-            if maze[r][c] == "O":
-                total += 100 * r + c
-    return total
+def calculate_score(warehouse):
+    score = 0
+    for row_index in range(len(warehouse)):
+        for col_index in range(len(warehouse[0])):
+            if warehouse[row_index][col_index] in "O[":
+                score += 100 * row_index + col_index
+    return score
 
-def expand_maze(maze):
-    new_maze = []
-    for row in maze:
-        expanded_row = []
-        for char in row:
-            if char == "#":
-                expanded_row.append("##")
-            elif char == "O":
-                expanded_row.append("[]")
-            elif char == ".":
-                expanded_row.append("..")
-            elif char == "@":
-                expanded_row.append("@.")
-        new_maze.append("".join(expanded_row))
-    return new_maze
+def double_warehouse_width(warehouse):
+    expanded = []
+    for row in warehouse:
+        new_row = []
+        for cell in row:
+            if cell == "#": new_row.extend(["#", "#"])
+            elif cell == "O": new_row.extend(["[", "]"])
+            elif cell == ".": new_row.extend([".", "."])
+            elif cell == "@": new_row.extend(["@", "."])
+        expanded.append("".join(new_row))
+    return expanded
 
-def part2(maze, path):
-    moves = {"^": (-1, 0), "v": (1, 0), "<": (0, -1), ">": (0, 1)}
-    maze = expand_maze(maze)
-    rr, cc = get_robot_pos(maze)
-    maze = [list(row) for row in maze]
-    maze[rr][cc] = "."
-    maze = ["".join(row) for row in maze]
+def part1(warehouse_layout, movement_sequence):
+    movement_directions = {"^": (-1, 0), "v": (1, 0), "<": (0, -1), ">": (0, 1)}
     
-    for step in path:
-        rr, cc, maze = move_robot(maze, rr, cc, *moves[step])
+    robot_row, robot_col = find_robot_position(warehouse_layout)
+    warehouse = [list(row) for row in warehouse_layout]
+    warehouse[robot_row][robot_col] = "."
+    warehouse = ["".join(row) for row in warehouse]
     
-    total = 0
-    for r in range(len(maze)):
-        for c in range(len(maze[0])):
-            if maze[r][c] == "[":
-                total += 100 * r + c
-    return total
+    for movement in movement_sequence:
+        direction_row, direction_col = movement_directions[movement]
+        robot_row, robot_col, warehouse = simulate_warehouse_movement(
+            warehouse, robot_row, robot_col, direction_row, direction_col
+        )
+    
+    return calculate_score(warehouse)
+
+def part2(warehouse_layout, movement_sequence):
+    movement_directions = {"^": (-1, 0), "v": (1, 0), "<": (0, -1), ">": (0, 1)}
+    
+    warehouse = double_warehouse_width(warehouse_layout)
+    robot_row, robot_col = find_robot_position(warehouse)
+    warehouse = [list(row) for row in warehouse]
+    warehouse[robot_row][robot_col] = "."
+    warehouse = ["".join(row) for row in warehouse]
+    
+    for movement in movement_sequence:
+        direction_row, direction_col = movement_directions[movement]
+        robot_row, robot_col, warehouse = simulate_warehouse_movement(
+            warehouse, robot_row, robot_col, direction_row, direction_col
+        )
+    
+    return calculate_score(warehouse)
+
+def main():
+    # Read input
+    lines = read_input_file_strip_lines("input.txt")
+    warehouse_layout, movement_sequence = parse_input(lines)
+    
+    # Part 1
+    start_time = time.time()
+    part_1 = part1(warehouse_layout, movement_sequence)
+    end_time = time.time()
+    print(f"Part 1: {part_1}")
+    print(f"Part 1 Execution Time: {end_time - start_time} seconds")
+    
+    # Part 2
+    start_time = time.time()
+    part_2 = part2(warehouse_layout, movement_sequence)
+    end_time = time.time()
+    print(f"Part 2: {part_2}")
+    print(f"Part 2 Execution Time: {end_time - start_time} seconds")
 
 if __name__ == "__main__":
-    maze, path = parse_input("input.txt")
-    print("Part 1:", part1(maze, path))
-    print("Part 2:", part2(maze, path))
+    main()
